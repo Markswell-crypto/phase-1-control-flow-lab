88b872f2a397994ca0003b77fde9885a09144a81
Markswell <osoromarkswell80@gmail.com>
2023-09-18 17:48:11 +0300

    First Else If Statement

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-  let ride = 199;
-
-  if(ride <= 400){
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
     return 'This one is on me!';
+  } else if (distance > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'Please input a valid distance.';
   }
-  
-  }
+}
 
 function ternaryCheckCity(){
   // Write your code here!
